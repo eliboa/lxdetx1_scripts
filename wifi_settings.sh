321abb71ecf3af911e695554d3d74614f405044b
eliboa <romain@laumonier.org>
2018-05-01 10:13:07 +0200

    bad path

--- wifi_settings.sh
+++ wifi_settings.sh
@@ -9,7 +9,7 @@ echo "Enter WiFi SSID (name) : "
 read ssid
 echo "Enter WiFi password : "
 read psk
-cat /etc/NetworkManager/system-connections/Gigaspot | sed s/ssid=.*/ssid=$ssid/ | sed s/psk=.*/psk=$psk/ > ../../etc/NetworkManager/system-connections/Gigaspot
+cat ../../etc/NetworkManager/system-connections/Gigaspot | sed s/ssid=.*/ssid=$ssid/ | sed s/psk=.*/psk=$psk/ > ../../etc/NetworkManager/system-connections/Gigaspot
 echo "2 lines edited in /etc/NetworkManager/system-connections/Gigaspot :"
-cat /etc/NetworkManager/system-connections/Gigaspot | grep 'ssid='
-cat /etc/NetworkManager/system-connections/Gigaspot | grep 'psk='
+cat ../../etc/NetworkManager/system-connections/Gigaspot | grep 'ssid='
+cat ../../etc/NetworkManager/system-connections/Gigaspot | grep 'psk='
